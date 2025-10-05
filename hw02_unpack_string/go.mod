35a58e098d6cdd1fe30077f1add127e41020ee96
Name <me@mail.com>
2025-10-05 22:52:15 +0400

    test on string literals passed TODO: UTF-8 and InvalidString

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/lamerkid/my-hw/hw02_unpack_string
 
 go 1.23
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,28 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var outputString strings.Builder
+	for i := 0; i < len(str); i++ {
+		n, err := strconv.Atoi(string(str[i]))
+		if i+1 < len(str) && string(str[i+1]) == "0" {
+			continue
+		}
+		if err != nil {
+			outputString.WriteString(string(str[i]))
+		} else {
+			if n == 0 {
+				outputString.WriteString(strings.Repeat(string(str[i-1]), n))
+			} else {
+				outputString.WriteString(strings.Repeat(string(str[i-1]), n-1))
+			}
+		}
+	}
+	return outputString.String(), nil
 }
