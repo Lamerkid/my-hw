23400d79f902dafbc0da0d7c59db17ebc8345bc2
Name <me@mail.com>
2025-10-06 07:11:44 +0400

    fix UTF-8 TODO: fix two digits in a row

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,18 +10,30 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	var outputString strings.Builder
-	for i := 0; i < len(str); i++ {
-		n, err := strconv.Atoi(string(str[i]))
-		if i+1 < len(str) && string(str[i+1]) == "0" {
+	runesArray := []rune(str)
+	for i := 0; i < len(runesArray); i++ {
+
+		if i+1 < len(runesArray) && string(runesArray[i+1]) == "0" {
 			continue
 		}
+
+		if i+1 < len(runesArray) && string(runesArray[i]) >= "0" && string(runesArray[i]) <= "9" && string(runesArray[i+1]) >= "0" && string(runesArray[i+1]) <= "9" {
+			return "", ErrInvalidString
+		}
+
+		n, err := strconv.Atoi(string(runesArray[i]))
+
 		if err != nil {
-			outputString.WriteString(string(str[i]))
+			outputString.WriteString(string(runesArray[i]))
 		} else {
+			if i == 0 {
+				return "", ErrInvalidString
+			}
+
 			if n == 0 {
-				outputString.WriteString(strings.Repeat(string(str[i-1]), n))
+				outputString.WriteString(strings.Repeat(string(runesArray[i-1]), n))
 			} else {
-				outputString.WriteString(strings.Repeat(string(str[i-1]), n-1))
+				outputString.WriteString(strings.Repeat(string(runesArray[i-1]), n-1))
 			}
 		}
 	}
