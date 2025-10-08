8bdb27e687ed7df9a6f2fbecaf4164e1090bc828
Name <me@mail.com>
2025-10-08 19:56:22 +0400

    refactor and add tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,29 +12,18 @@ func Unpack(str string) (string, error) {
 	var outputString strings.Builder
 	runesArray := []rune(str)
 	for i := 0; i < len(runesArray); i++ {
-
-		if i+1 < len(runesArray) && string(runesArray[i]) >= "0" && string(runesArray[i]) <= "9" && string(runesArray[i+1]) >= "0" && string(runesArray[i+1]) <= "9" {
-			return "", ErrInvalidString
-		}
-
-		if i+1 < len(runesArray) && string(runesArray[i+1]) == "0" {
-			continue
-		}
-
-		n, err := strconv.Atoi(string(runesArray[i]))
-
-		if err != nil {
-			outputString.WriteString(string(runesArray[i]))
-		} else {
-			if i == 0 {
+		// Если успешное преобразование в int, то записываем предыдущую руну n раз
+		if n, err := strconv.Atoi(string(runesArray[i])); err == nil {
+			// Если n первая в строке или число двузначное, то возвращаем пустую строку и ошибку
+			if i == 0 || i+1 < len(runesArray) && string(runesArray[i+1]) >= "0" && string(runesArray[i+1]) <= "9" {
 				return "", ErrInvalidString
 			}
 
-			if n == 0 {
-				outputString.WriteString(strings.Repeat(string(runesArray[i-1]), n))
-			} else {
-				outputString.WriteString(strings.Repeat(string(runesArray[i-1]), n-1))
-			}
+			outputString.WriteString(strings.Repeat(string(runesArray[i-1]), n))
+
+			// Если после руны нет числа, то записываем эту руну
+		} else if !(i+1 < len(runesArray) && string(runesArray[i+1]) >= "0" && string(runesArray[i+1]) <= "9") {
+			outputString.WriteString(string(runesArray[i]))
 		}
 	}
 	return outputString.String(), nil
