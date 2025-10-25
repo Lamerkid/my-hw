ec97719618813179ffb99e902fc3aef442374db5
Name <me@mail.com>
2025-10-26 01:43:52 +0400

    assignment done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	if len(str) == 0 {
+		return nil
+	}
+
+	wordsSlice := strings.Fields(str)
+	wordsMap := map[string]int{}
+
+	for _, word := range wordsSlice {
+		if val, ok := wordsMap[word]; ok {
+			wordsMap[word] = val + 1
+			continue
+		}
+		wordsMap[word] = 1
+	}
+
+	type record struct {
+		Word  string
+		Count int
+	}
+
+	records := make([]record, 0, len(wordsSlice))
+	for k, v := range wordsMap {
+		records = append(records, record{k, v})
+	}
+
+	sort.Slice(records, func(i, j int) bool {
+		if records[i].Count == records[j].Count {
+			return records[i].Word < records[j].Word
+		}
+		return records[i].Count > records[j].Count
+	})
+
+	var output []string
+	for i := 0; i < 10; i++ {
+		output = append(output, records[i].Word)
+	}
+
+	return output
 }
