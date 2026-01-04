9cc1d32e69188fa8c186570a2d3312fd9da4e8e8
Name <me@mail.com>
2026-01-04 23:20:06 +0400

    validator done

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,13 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +16,152 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	err := strings.Builder{}
+	for _, v := range v {
+		err.WriteString(v.Err.Error() + ", ")
+	}
+	return err.String()
 }
 
+var (
+	ErrNotAStruct = errors.New("not a struct")
+
+	ErrValidateStringLen    = errors.New("string not correct length")
+	ErrValidateStringRegexp = errors.New("string not correct regexp")
+	ErrValidateStringIn     = errors.New("string is not in array")
+	ErrValidateIntMin       = errors.New("integer less than minimum")
+	ErrValidateIntMax       = errors.New("integer more than maximum")
+	ErrValidateIntIn        = errors.New("integer is not in array")
+)
+
 func Validate(v interface{}) error {
-	// Place your code here.
+	reflVal := reflect.ValueOf(v)
+	reflType := reflect.TypeOf(v)
+
+	var errs ValidationErrors
+
+	if reflVal.Kind() != reflect.Struct {
+		return ErrNotAStruct
+	}
+
+	for i := 0; i < reflVal.NumField(); i++ {
+		fieldName := reflVal.Type().Field(i).Name
+		value := reflVal.Field(i)
+		tag := reflType.Field(i).Tag.Get("validate")
+
+		if tag == "" {
+			continue
+		}
+
+		parts := strings.Split(tag, "|")
+		for _, part := range parts {
+			err := validateStruct(value, part)
+			if err != nil {
+				errs = append(errs, ValidationError{
+					Field: fieldName,
+					Err:   err,
+				})
+			}
+		}
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func validateStruct(field reflect.Value, tag string) error {
+	parts := strings.Split(tag, ":")
+	tagKey, tagValue := parts[0], parts[1]
+
+	switch field.Kind() { //nolint:exhaustive
+	case reflect.String:
+		return validateString(field, tagKey, tagValue)
+	case reflect.Int:
+		return validateInt(field, tagKey, tagValue)
+	case reflect.Slice:
+		return validateSlice(field, tag)
+	}
+	return nil
+}
+
+func validateString(field reflect.Value, tagKey, tagValue string) error {
+	switch tagKey {
+	case "len":
+		val, err := strconv.Atoi(tagValue)
+		if err != nil {
+			return err
+		}
+		if len(field.String()) != val {
+			return ErrValidateStringLen
+		}
+		return nil
+	case "regexp":
+		reg, err := regexp.Compile(tagValue)
+		if err != nil {
+			return err
+		}
+		if !reg.MatchString(field.String()) {
+			return ErrValidateStringRegexp
+		}
+		return nil
+	case "in":
+		arrayVals := strings.Split(tagValue, ",")
+		for _, val := range arrayVals {
+			if field.String() == val {
+				return nil
+			}
+		}
+		return ErrValidateStringIn
+	}
+	return nil
+}
+
+func validateInt(field reflect.Value, tagKey, tagValue string) error {
+	switch tagKey {
+	case "min":
+		val, err := strconv.Atoi(tagValue)
+		if err != nil {
+			return err
+		}
+		if int(field.Int()) < val {
+			return ErrValidateIntMin
+		}
+		return nil
+	case "max":
+		val, err := strconv.Atoi(tagValue)
+		if err != nil {
+			return err
+		}
+		if int(field.Int()) > val {
+			return ErrValidateIntMax
+		}
+		return nil
+	case "in":
+		arrayVals := strings.Split(tagValue, ",")
+		for _, val := range arrayVals {
+			val, err := strconv.Atoi(val)
+			if err != nil {
+				return err
+			}
+			if int(field.Int()) == val {
+				return nil
+			}
+		}
+		return ErrValidateIntIn
+	}
+	return nil
+}
+
+func validateSlice(field reflect.Value, tag string) error {
+	for i := 0; i < field.Len(); i++ {
+		err := validateStruct(field.Index(i), tag)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
