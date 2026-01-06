91e00c79098af89c6fe930147799b240e2529312
Name <me@mail.com>
2026-01-06 20:59:14 +0400

    one func

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -24,26 +24,18 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsersDomains(r, domain)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return u, nil
-}
-
-func getUsersDomains(r io.Reader, domain string) (result DomainStat, err error) {
 	scanner := bufio.NewScanner(r)
-	result = make(DomainStat)
+	result := make(DomainStat)
 
 	user := User{}
 
 	for scanner.Scan() {
-		if err = easyjson.Unmarshal(scanner.Bytes(), &user); err != nil {
-			return
+		if err := easyjson.Unmarshal(scanner.Bytes(), &user); err != nil {
+			return nil, fmt.Errorf("unmarshaling error: %w", err)
 		}
 		if strings.Contains(user.Email, "."+domain) {
 			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
 		}
 	}
-	return
+	return result, nil
 }
