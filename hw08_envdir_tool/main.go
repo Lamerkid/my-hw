8a1b4327433563c7ee3f2a0090076a414f0671ad
Name <me@mail.com>
2026-01-08 15:03:44 +0400

    os.Exit

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -8,20 +8,21 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		slog.Error("No directory path to read environment variables is specified.")
-		return
+		os.Exit(1)
 	}
 
 	if len(os.Args) < 3 {
 		slog.Error("No command to execute is specified.")
-		return
+		os.Exit(1)
 	}
 
 	env, err := ReadDir(os.Args[1])
 	if err != nil {
 		slog.Error("error parsing envronment variables", "error", err)
+		os.Exit(1)
 	}
 
 	returnCode := RunCmd(os.Args[2:], env)
 
-	slog.Info("Program exited with", "code", returnCode)
+	os.Exit(returnCode)
 }
